88b45d16e3b3468e0bcb953d7336615c0a1a8466
Rajat Khandelwal <rajatkhandelwal8013@gmail.com>
2024-03-16 18:00:03 +0530

    Updated Styling

--- src/Components/AcceptanceCriteriaForm.js
+++ src/Components/AcceptanceCriteriaForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Grid, Paper, Typography, TextField } from "@mui/material";
 
 import companyLogo from "../Images/Vwits Logo.jpeg";
